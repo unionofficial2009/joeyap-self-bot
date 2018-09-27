9d17eb805af0f3e0f3b9a0369c5a085c8b0d3393
unionofficial2009 <38653752+unionofficial2009@users.noreply.github.com>
2018-09-27 10:43:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,21 +3,46 @@ const bot = new Discord.Client();
 const superagent = require("superagent");
 //const randomWord = require("random-words")
  
-
+var spam;
  
-
+var phrases = ["thing1","another one","keep adding more","there's no limit"];
+ 
+function startspam()
+{
+    console.log("Spam starting!")
+    var server = bot.guilds.get("427076307596345344");
+    var chan = new discord.TextChannel(server,{"id":"427076307596345346"});
+    spam = bot.setInterval(()=>
+    {
+        chan.send("test").then(msg=>{ // Sticking with randomwords.
+            console.log(msg.content);
+        });
+  
+    },5000);
+}
+ 
+function stopspam()
+{
+    bot.clearTimeout(spam);
+    console.log("Spam Stopped.")
+}
  
 bot.on("ready",()=>{
     console.log("Ready!");
-    var server = bot.guilds.find("id","431118123664670720")
-    var chan = new discord.TextChannel(server,{"id":"487330979908681729"});
-    bot.setInterval(()=>
-    {
-        chan.send("test").then(msg=>{
-            console.log(msg.content);
-        });
+})
  
-    },60000);
+bot.on("message",msg=>{
+    if(msg.author.id == "374886124126208000")
+    {
+        if(msg.content.toLowerCase() == "=start")
+        {
+            startspam()
+        }
+        else if(msg.content.toLowerCase() == "=stop")
+        {
+            stopspam()
+        }
+    }
 })  
 
 
