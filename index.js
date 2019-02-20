9c52e34ed9fc497e3140191ca85a70d6bafda5cb
unionofficial2009 <38653752+unionofficial2009@users.noreply.github.com>
2019-02-20 16:33:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,33 +8,33 @@ var spam;
  
 
  
-function startspam()
-{
+//function startspam()
+//{
  
-    console.log("Spam starting!")
-    var server = bot.guilds.get("431118123664670720");
-    var chan = new discord.TextChannel(server,{"id":"487330979908681729"});
-    spam = bot.setInterval(()=>
-    {       
+ //   console.log("Spam starting!")
+//    var server = bot.guilds.get("431118123664670720");
+ //   var chan = new discord.TextChannel(server,{"id":"487330979908681729"});
+//    spam = bot.setInterval(()=>
+//    {       
        
-       chan.send(randomWord()).then(msg=>{
-            console.log(msg.content);
-        }).catch((err) => {
-            chan.send("=start").then(msg=>{
-            console.log(msg.content);
-          }); 	   
-      });	  
+//       chan.send(randomWord()).then(msg=>{
+//            console.log(msg.content);
+ //       }).catch((err) => {
+ //           chan.send("=start").then(msg=>{
+ //           console.log(msg.content);
+ //         }); 	   
+ //     });	  
   
-    },180000);
-}
+//    },180000);
+//}
 
 
  
-function stopspam()
-{
-    bot.clearTimeout(spam);
-    console.log("Spam Stopped.");
-}
+//function stopspam()
+//{
+//    bot.clearTimeout(spam);
+//    console.log("Spam Stopped.");
+//}
  
 bot.on("ready",()=>{
     console.log("Ready!");
@@ -61,19 +61,19 @@ bot.on("ready",()=>{
  
 });
  
-bot.on("message",msg=>{
-    if(msg.author.id == "421634731709562886")
-    {
-        if(msg.content.toLowerCase() == "=start")
-        {
-            startspam();
-        }
-        else if(msg.content.toLowerCase() == "=stop")
-        {
-            stopspam();
-        }
-    }
-});
+//bot.on("message",msg=>{
+//    if(msg.author.id == "421634731709562886")
+//    {
+//        if(msg.content.toLowerCase() == "=start")
+//        {
+//            startspam();
+//        }
+//        else if(msg.content.toLowerCase() == "=stop")
+//        {
+ //           stopspam();
+//        }
+//    }
+//});
 
 
 
